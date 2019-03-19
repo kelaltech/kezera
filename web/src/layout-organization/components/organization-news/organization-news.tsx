4c164eb7b1f14b6abbac0644e8172df7ceefb777
Biruk <biruktesfayeve@gmail.com>
2019-03-19 12:50:10 +0300

    news component added

---
+++ web/src/layout-organization/components/organization-news/organization-news.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+import './default-promotion.scss'
+
+export default class OrganizationNews extends Component<{}, {}> {
+  render() {
+    return <div>News Card</div>
+  }
+}
