186ac3aa9d567f4d4798d751a6a58c040ac69072
Biruk <biruktesfayeve@gmail.com>
2019-03-19 10:23:11 +0300

    comment model added

--- api/models/news/news.path.ts
+++ api/models/news/news.path.ts
@@ -3,7 +3,7 @@ import { SchemaDefinition, Schema } from 'mongoose'
 const ObjectId = Schema.Types.ObjectId
 
 export const newPaths: SchemaDefinition = {
-  _at: { type: Date, defalut: Date.now },
+  _at: { type: Date, default: Date.now },
   title: { type: String, required: true },
   description: { type: String, max: 20, required: true },
   article: { type: String },
