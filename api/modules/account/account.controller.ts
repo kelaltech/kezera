a746b37dde4b334bfcf8656dc7475fdc180b0f98
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-03-21 13:07:22 +0300

    Add account functionality

--- api/modules/account/account.controller.ts
+++ api/modules/account/account.controller.ts
@@ -1,10 +1,32 @@
 import { ClientSession } from 'mongoose'
 
 import { KoaController } from '../../lib/koa-controller'
+import { IAccountRequest, IAccountResponse } from './account.apiv'
+import { accountDocumentToResponse, accountRequestToDocument } from './account.filter'
+import {
+  AccountModel,
+  IAccountRole,
+  IAccountStatus
+} from '../../models/account/account.model'
+import { add } from '../../lib/crud'
 
 export class AccountController extends KoaController {
-  async test(session: ClientSession): Promise<string> {
-    console.log('In transaction?', session.inTransaction())
-    return 'Hello, World!'
+  async add(
+    session: ClientSession | undefined,
+    data = super.getRequestBody<IAccountRequest>(),
+    role: IAccountRole = 'VOLUNTEER',
+    status: IAccountStatus = 'ACTIVE'
+  ): Promise<IAccountResponse> {
+    let document = await accountRequestToDocument(data, role, status)
+
+    await add(AccountModel, document, {
+      session,
+      preSave: async doc => {
+        await doc.setPassword(data.password)
+        return doc
+      }
+    })
+
+    return accountDocumentToResponse(document)
   }
 }
--- api/modules/account/account.router.ts
+++ api/modules/account/account.router.ts
@@ -2,18 +2,21 @@ import * as Router from 'koa-router'
 
 import { handle } from '../../lib/middlewares/handle'
 import { AccountController } from './account.controller'
-import { authorize } from '../../lib/middlewares/authorize'
 import { login } from '../../lib/middlewares/login'
 import { logout } from '../../lib/middlewares/logout'
 
 export const accountRouter = new Router({ prefix: '/api/account' })
 
-// GET /api/account/test // todo: temp
-accountRouter.get(
-  '/test',
-  authorize('ADMIN'),
-  handle(AccountController, (c, s) => c.test(s))
-)
+/* GENERAL */
+
+// POST /api/account/add
+accountRouter.post('/add', handle(AccountController, (c, s) => c.add(s)))
+
+/* ACCOUNT RESET */
+
+// todo
+
+/* BASIC AUTH */
 
 // POST /api/account/login
 accountRouter.post('/login', login({ strategyName: 'local' }))
