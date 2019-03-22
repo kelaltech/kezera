e409d3914f1c8a5ce045863c02615f9f0f839f9f
Biruk <biruktesfayeve@gmail.com>
2019-03-22 12:12:01 +0300

    comment module added

--- api/modules/news/news.controller.ts
+++ api/modules/news/news.controller.ts
@@ -1,5 +1,6 @@
-import { add, get, list } from '../../lib/crud'
+import { add, edit, get, list, remove, search } from '../../lib/crud'
 import { NewsModel } from '../../models/news/news.model'
+import { commentModel } from '../../models/comment/comment.model'
 import { IAccount } from '../../models/account/account.model'
 import { Schema } from 'mongoose'
 
@@ -42,4 +43,52 @@ export async function toggleLike(
   }
 
   return { likes: doc.likes.length }
+}
+
+export async function removeNews(_newsId: ObjectId): Promise<any> {
+  const docs = await remove(NewsModel, _newsId)
+  return docs
+}
+
+export async function getNews(_newsId: ObjectId): Promise<any> {
+  const docs = await get(NewsModel, _newsId)
+  return docs
+}
+
+export async function editNews(data: any, _newsId: ObjectId): Promise<any> {
+  const doc = await edit(NewsModel, _newsId, data)
+
+  return doc
+}
+
+export async function searchNews(term: string): Promise<any> {
+  return search(NewsModel, term)
+}
+
+/*export async function addNewsWithPicture(
+  data: any,
+  orgId: Schema.Types.ObjectId,
+  pic: Stream
+):Promise<any>{
+
+  data._by = await orgId
+  const news = await add(NewsModel,data)
+  const grid = new Grid (serverApp, NewsModel, news._id)
+
+//TODO
+
+}*/
+
+export async function addComment(
+  account: IAccount,
+  _newsId: ObjectId,
+  data: any
+): Promise<any> {
+  data._by = await account._id
+  const comment = await add(commentModel, data)
+  const news = await get(NewsModel, _newsId)
+
+  news.comments.push(comment._id)
+  await news.save()
+  return comment
 }
--- api/modules/news/news.route.ts
+++ api/modules/news/news.route.ts
@@ -1,15 +1,27 @@
 import Route = require('koa-router')
 
-import { addNews, getAllNews, toggleLike } from './news.controller'
+import {
+  addComment,
+  getAllNews,
+  removeNews,
+  editNews,
+  searchNews,
+  toggleLike,
+  getNews
+} from './news.controller'
 
 export const newsRouter = new Route({
   prefix: '/api/news'
 })
 
 // POST /api/news/new
-newsRouter.post('/new', async ctx => {
-  ctx.body = await addNews(ctx.request.body, ctx.state.user)
-})
+/*newsRouter.post('/new', async ctx => {
+  ctx.body = await addNewsWithPicture(
+    ctx.request.body,
+    ctx.state.user,
+    fs.createReadStream(ctx.request.files!.picture.path)
+  )
+})*/
 
 // GET /api/news/list?since&count
 newsRouter.get('/list', async ctx => {
@@ -17,9 +29,33 @@ newsRouter.get('/list', async ctx => {
 })
 
 //PUT /api/news/:_newsID/like
-
 newsRouter.put('/:_newsId/like', async ctx => {
   //return a number
 
   ctx.body = await toggleLike(ctx.query._newsId, ctx.state.user)
+})
+
+//DELETE /api/news/:_newsId
+newsRouter.delete('/:newsId', async ctx => {
+  ctx.body = await removeNews(ctx.query._newsId)
+})
+
+//GET /api/news/:_newsId
+newsRouter.get('/:newsId', async ctx => {
+  ctx.body = await getNews(ctx.query._newsId)
+})
+
+//PUT /api/news/:_newsId   edit
+newsRouter.put('/:_newsId', async ctx => {
+  ctx.body = await editNews(ctx.request.body, ctx.query._newsId)
+})
+
+//GET /api/news/search?term=:term
+newsRouter.get('/search', async ctx => {
+  ctx.body = await searchNews(ctx.params.term)
+})
+//POST /api/news/:_newsId/comment/new
+
+newsRouter.post('/:_newsId/comment/new', async ctx => {
+  ctx.body = await addComment(ctx.state.user, ctx.params._newsId, ctx.request.body)
 })
