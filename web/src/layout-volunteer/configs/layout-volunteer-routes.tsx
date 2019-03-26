d60dd97f81cae0b0ebca1ba8a3639cc1e1997c8b
Biruk <biruktesfayeve@gmail.com>
2019-03-26 09:48:38 +0300

    volunteer pages routes configured

--- web/src/layout-volunteer/configs/layout-volunteer-routes.tsx
+++ web/src/layout-volunteer/configs/layout-volunteer-routes.tsx
@@ -12,6 +12,19 @@ const AccountDetail = lazy(() =>
 const AccountVolunteerRegister = lazy(() =>
   import('../pages/volunteer-register/volunteer-register')
 )
+const Discovery = lazy(() => import('../pages/volunteer-discover/volunteer-discovery'))
+const Event = lazy(() => import('../pages/volunteer-event/volunteer-event'))
+const Landing = lazy(() => import('../pages/volunteer-landing/volunteer-landing'))
+const MyOrganization = lazy(() =>
+  import('../pages/volunteer-my-organization/volunteer-my-organization')
+)
+const News = lazy(() => import('../pages/volunteer-news/volunteer-news'))
+const Profile = lazy(() => import('../pages/volunteer-profile/volunteer-profile'))
+const Request = lazy(() => import('../pages/volunteer-request/volunteer-request'))
+const SearchResult = lazy(() =>
+  import('../pages/volunteer-search-result/volunteer-search-result')
+)
+const Task = lazy(() => import('../pages/volunteer-task/volunteer-task'))
 
 export default function LayoutVolunteerRoutes({ prefix: p }: { prefix: string }) {
   const { account } = useAccountState()
@@ -19,7 +32,6 @@ export default function LayoutVolunteerRoutes({ prefix: p }: { prefix: string })
   return (
     <Switch>
       {account && <Redirect from={`${p}/register`} to={`/login/redirect/account`} />}
-
       {account ? (
         <Route exact path={`${p}/account`} component={AccountDetail} />
       ) : (
@@ -29,9 +41,17 @@ export default function LayoutVolunteerRoutes({ prefix: p }: { prefix: string })
           to={`/login?${qs.stringify({ continue: `${p}/account` })}`}
         />
       )}
-
       <Route exact path={`${p}/register`} component={AccountVolunteerRegister} />
-
+      <Route exact path={`${p}/discovery`} component={Discovery} />
+      <Route exact path={`${p}/events`} component={Event} />
+      <Route exact path={`${p}/landing`} component={Landing} /> //todo change the path to
+      '/'
+      <Route exact path={`${p}/my-organization`} component={MyOrganization} />
+      <Route exact path={`${p}/news`} component={News} />
+      <Route exact path={`${p}/profile`} component={Profile} />
+      <Route exact path={`${p}/request`} component={Request} />
+      <Route exact path={`${p}/search-result`} component={SearchResult} />
+      <Route exact path={`${p}/task`} component={Task} />
       <Route component={NotFound} />
     </Switch>
   )
