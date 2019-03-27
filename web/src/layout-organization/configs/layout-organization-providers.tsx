6f6c195a92f0df2324404a7cf6f2c6eb72aea9eb
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-03-27 22:08:15 +0300

    Partially defined web/src/shared/stores/organizations/*

--- web/src/layout-organization/configs/layout-organization-providers.tsx
+++ web/src/layout-organization/configs/layout-organization-providers.tsx
@@ -1,7 +1,12 @@
 import React, { PropsWithChildren } from 'react'
 
 import { MyOrganizationProvider } from '../stores/my-organization/my-organization-provider'
+import { OrganizationsProvider } from '../../shared/stores/organizations/organizations-provider'
 
 export default function LayoutOrganizationProviders({ children }: PropsWithChildren<{}>) {
-  return <MyOrganizationProvider>{children}</MyOrganizationProvider>
+  return (
+    <OrganizationsProvider>
+      <MyOrganizationProvider>{children}</MyOrganizationProvider>
+    </OrganizationsProvider>
+  )
 }
