0094154d8895c4d05fb05f6ee99f9e1dee051f89
Biruk <biruktesfayeve@gmail.com>
2019-03-31 11:40:54 +0300

    my organization page ui completed waiting for api to connect to back

--- web/src/layout-volunteer/pages/volunteer-event/volunteer-event.tsx
+++ web/src/layout-volunteer/pages/volunteer-event/volunteer-event.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Block } from 'gerami'
-import VolunteerEventCard from '../../components/volunteer-event-card/volunteer-event-card'
 import ProductSlider from '../../components/volunteer-product-slider/volunteer-product-slider'
 
 import './volunteer-event.scss'
@@ -10,57 +9,56 @@ function VolunteerEvents() {
   const [events, setEvents] = useState([])
   const [recentEvents, setRecentEvents] = useState([])
 
-  useEffect(() => {
+  useEffect(()=>{
     axios
       .get('/api/event/all')
-      .then(events => {
+      .then((events)=>{
         setEvents(events.data)
       })
-      .catch(e => console.log(e))
+      .catch(e=>console.log(e))
 
     axios
       .get('/api/event/recent') //todo change the api request
-      .then(events => {
+      .then((events)=>{
         setRecentEvents(events.data)
       })
-      .catch(e => console.log(e))
-  }, [])
+      .catch(e=>console.log(e))
+
+  },[])
   return (
     <div className={'events-container'}>
       <div className={'events-around-you'}>
         <h4>Events Near you</h4>
         <Block />
-        {events.map((event: any) => (
+        {events.map((event:any)=>(
           <div>
-            <Block />
-            <EventCard event={event} role={'VOLUNTEER'} fetch={() => {}} />
-            <Block />
+          <Block />
+          <EventCard event={event}  role={'VOLUNTEER'} fetch={()=>{}}/>
+          <Block/>
           </div>
         ))}
-        <Block />
+        <Block/>
       </div>
       <div className={'events-list-container'}>
         <h4>Coming up..! </h4>
         <div>
-          <ProductSlider sliderWidth={events.length * 50 + '%'}>
-            {events.map((event: any) => (
-              <div
-                style={{
-                  padding: '0 13px'
-                }}
-              >
-                <EventCard event={event} role={'VOLUNTEER'} fetch={() => {}} />
-              </div>
+          <ProductSlider sliderWidth={events.length*50+'%'}>
+            {events.map((event:any)=>(
+           <div style={{
+             padding: '0 13px'
+           }}>
+                <EventCard event={event}  role={'VOLUNTEER'} fetch={()=>{}}/>
+           </div>
             ))}
           </ProductSlider>
         </div>
       </div>
-      <Block />
+      <Block/>
       <div className={'events-list-container'}>
         <h4>Upcoming </h4>
-        {events.map((event: any) => (
+        {events.map((event:any)=>(
           <Block>
-            <EventCard event={event} role={'VOLUNTEER'} fetch={() => {}} />
+            <EventCard event={event}  role={'VOLUNTEER'} fetch={()=>{}}/>
           </Block>
         ))}
       </div>
