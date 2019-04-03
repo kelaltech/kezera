bb41e1632aacd8447c0438508508dbcfd1bbb3ac
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-04-03 18:23:38 +0300

    Add VerifierController.approveOrganizationApplication()

---
+++ api/modules/verifier/verifier.router.ts
@@ -0,0 +1,14 @@
+import * as Router from 'koa-router'
+
+import { handle } from '../../lib/middlewares/handle'
+import { authorize } from '../../lib/middlewares/authorize'
+import { VerifierController } from './verifier.controller'
+
+export const verifierRouter = new Router({ prefix: '/api/verifier' })
+
+// POST /api/verifier/approve-organization-application/:_id
+verifierRouter.post(
+  '/approve-organization-application/:_id',
+  authorize(['VERIFIER']),
+  handle(VerifierController, (c, s) => c.approveOrganizationApplication(s))
+)
