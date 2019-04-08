ae9695a5e4d32899ed817593a93337ff806c2506
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-04-08 12:09:47 +0300

    Add optional image `covers` and `photo` to RichPage

--- web/src/shared/components/rich-page/rich-page.tsx
+++ web/src/shared/components/rich-page/rich-page.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useEffect } from 'react'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
 import { Block, Content, geramiSizeTypes, Loading, Page, Warning } from 'gerami'
 
 import './rich-page.scss'
@@ -15,6 +15,10 @@ type Props = PropsWithChildren<{
 
   documentTitle?: string // default to Props.title, if it is string
   documentTitleOptions?: IUseTitleOptions
+
+  covers?: string[]
+  coverDuration?: number
+  photo?: string
 
   title?: string | JSX.Element
   description?: string | JSX.Element
@@ -42,6 +46,10 @@ function RichPage({
   documentTitle,
   documentTitleOptions,
 
+  covers = [],
+  coverDuration = 14000,
+  photo,
+
   title,
   description,
 
@@ -55,42 +63,80 @@ function RichPage({
     documentTitleOptions
   )
 
+  const [coverIndex, setCoverIndex] = useState(-1)
+
+  useEffect(() => {
+    let interval: any
+    if (covers.length) {
+      if (coverIndex === -1) setCoverIndex(0)
+      interval = setInterval(() => {
+        setCoverIndex(coverIndex + 1 < covers.length ? coverIndex + 1 : 0)
+      }, coverDuration)
+    }
+    return () => (interval ? clearInterval(interval) : undefined)
+  }, [coverIndex])
+
+  useEffect(() => {
+    // prefetch all cover images into the browser's cache
+    for (const cover of covers) {
+      const img = document.createElement('img')
+      img.src = cover
+    }
+  }, [])
+
   return !(ready !== false && loaded) ? (
     <Loading />
   ) : (
-    <Page>
-      <Content size={size} transparent style={{ overflow: 'visible' }}>
-        {title === undefined ? null : (
-          <>
-            <Block first className={`padding-horizontal-none`}>
-              {typeof title === 'string' ? <h1>{title}</h1> : title}
+    <>
+      {(coverIndex !== -1 || photo !== undefined) && (
+        <div
+          className={'rich-page-cover bg-whitish'}
+          style={
+            coverIndex === -1 ? {} : { backgroundImage: `url(${covers[coverIndex]})` }
+          }
+        />
+      )}
+      {photo && (
+        <Content
+          className={'rich-page-photo'}
+          style={{ backgroundImage: `url(${photo})` }}
+        />
+      )}
+
+      <Page>
+        <Content size={size} transparent style={{ overflow: 'visible' }}>
+          {title === undefined ? null : (
+            <>
+              <Block first className={`padding-horizontal-none`}>
+                {typeof title === 'string' ? <h1>{title}</h1> : title}
+              </Block>
+
+              <div className={`padding-horizontal-none`}>
+                <hr />
+              </div>
+            </>
+          )}
+
+          {description === undefined ? null : (
+            <Block last className={`padding-horizontal-none font-S fg-blackish`}>
+              {description}
             </Block>
+          )}
 
-            <div className={`padding-horizontal-none`}>
-              <hr />
-            </div>
-          </>
-        )}
+          {!error ? null : (
+            <Warning
+              className={`margin-bottom-very-big padding-horizontal-none font-S`}
+              problem={
+                typeof error === 'string' ? error : error.prettyMessage || error.message
+              }
+              shy={() => onErrorClose && onErrorClose(undefined)}
+            />
+          )}
 
-        {description === undefined ? null : (
-          <Block last className={`padding-horizontal-none font-S fg-blackish`}>
-            {description}
-          </Block>
-        )}
-
-        {!error ? null : (
-          <Warning
-            className={`margin-bottom-very-big padding-horizontal-none font-S`}
-            problem={
-              typeof error === 'string' ? error : error.prettyMessage || error.message
-            }
-            shy={() => onErrorClose && onErrorClose(undefined)}
-          />
-        )}
-
-        <div className={'rich-page-content'}>{children}</div>
-      </Content>
-    </Page>
+          <div className={'rich-page-content'}>{children}</div>
+        </Content>
+      </Page>
+    </>
   )
 }
 
