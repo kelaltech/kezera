d512a07daf6ccf40e90457467f1d96dae41f22b7
Biruk <biruktesfayeve@gmail.com>
2019-04-13 10:20:46 +0300

    profile page is responsive now

--- web/src/layout-volunteer/pages/volunteer-profile/volunteer-profile.tsx
+++ web/src/layout-volunteer/pages/volunteer-profile/volunteer-profile.tsx
@@ -40,8 +40,34 @@ function Profile() {
     autoplay: true,
     speed: 2000,
     autoplaySpeed: 3000,
-    cssEase: "linear"
-  };
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
   const {account} = useAccountState()
   const {volunteer} = useVolunteerState()
   const [events, setEvents] = useState([])
