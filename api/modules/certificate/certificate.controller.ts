96909ca3a1be4e855b0e4de28ebbaccfd2b241d3
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-04-24 10:56:40 +0300

    Define CertificateController.list

--- api/modules/certificate/certificate.controller.ts
+++ api/modules/certificate/certificate.controller.ts
@@ -1,5 +1,49 @@
+import { ClientSession } from 'mongoose'
+import { Stream } from 'stream'
+
 import { KoaController } from '../../lib/koa-controller'
+import { ICertificateResponse } from './certificate.apiv'
+import { get, list } from '../../lib/crud'
+import { CertificateModel } from '../../models/certificate/certificate.model'
+import { VolunteerModel } from '../../models/volunteer/volunteer.model'
+import { certificateDocumentToResponse } from './certificate.filter'
 
 export class CertificateController extends KoaController {
+  /* ISSUES */
+
+  async issue(session?: ClientSession): Promise<ICertificateResponse> {
+    // todo
+    throw Error(JSON.stringify(session))
+  }
+
   /* GENERAL */
+
+  async list(
+    session?: ClientSession,
+    volunteer_id = super.getParam('volunteer_id'),
+    account = super.getUser()
+  ): Promise<ICertificateResponse[]> {
+    const volunteer = await get(VolunteerModel, volunteer_id, { session })
+
+    const certificates = await list(CertificateModel, {
+      session,
+      conditions: {
+        issuedTo: volunteer._id,
+        public: !(account && account._id.toString() === volunteer.account.toString())
+      }
+    })
+    return await Promise.all(
+      certificates.map(certificate => certificateDocumentToResponse(certificate))
+    )
+  }
+
+  async print(session?: ClientSession): Promise<Stream> {
+    // todo
+    throw Error(JSON.stringify(session))
+  }
+
+  async setPrivacy(session?: ClientSession): Promise<ICertificateResponse[]> {
+    // todo
+    throw Error(JSON.stringify(session))
+  }
 }
--- api/modules/certificate/certificate.router.ts
+++ api/modules/certificate/certificate.router.ts
@@ -2,9 +2,33 @@ import * as Router from 'koa-router'
 import * as sharp from 'sharp'
 import * as fs from 'fs'
 
+import { handle } from '../../lib/middlewares/handle'
+import { CertificateController } from './certificate.controller'
+import { authorize } from '../../lib/middlewares/authorize'
+
 export const certificateRouter = new Router({ prefix: '/api/certificate' })
 
+/* ISSUES */
+
+// todo
+
 /* GENERAL */
+
+// GET /api/certificate/list/:account_id
+certificateRouter.get(
+  '/list/:account_id',
+  handle(CertificateController, (c, s) => c.list(s))
+)
+
+// GET /api/certificate/print/:_id
+certificateRouter.get('/print/:_id', handle(CertificateController, (c, s) => c.print(s)))
+
+// PUT /api/certificate/set-privacy/:_id
+certificateRouter.get(
+  '/set-privacy/:_id',
+  authorize(['VOLUNTEER']),
+  handle(CertificateController, (c, s) => c.setPrivacy(s))
+)
 
 /* TODO: TEMP */
 
