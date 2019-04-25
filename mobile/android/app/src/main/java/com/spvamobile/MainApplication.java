22b8c7245f5d8cfe69d6b719f078921568e974ac
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-04-25 11:15:09 +0300

    Configure react-native-elements and react-native-vector-icons

--- mobile/android/app/src/main/java/com/spvamobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/spvamobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.spvamobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new RNCWebViewPackage(),
             new AsyncStoragePackage()
