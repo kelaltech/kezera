ef31601e17ffa3cc64e12bcbad91dce54bb26463
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-05-08 18:03:31 +0300

    Mobile: install and link react-native-image-picker

--- mobile/android/app/src/main/java/com/spvamobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/spvamobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.spvamobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNScreensPackage(),
             new ReactNativeConfigPackage(),
             new VectorIconsPackage(),
