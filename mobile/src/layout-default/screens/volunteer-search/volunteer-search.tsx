33e1384c4eb0eeafcbf2f5e757efbb0e77829efc
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-05-24 20:46:05 +0300

    Stop static caching /service-worker.js to ensure timely updates in production-time PWAs

--- mobile/src/layout-default/screens/volunteer-search/volunteer-search.tsx
+++ mobile/src/layout-default/screens/volunteer-search/volunteer-search.tsx
@@ -12,106 +12,97 @@ import { richTextToDisplayText } from '../../../lib/richTextConverter'
 function VolunteerSearch({  }: NavigationInjectedProps<{}>) {
   const { loading, t } = useLocale(['volunteer'])
   const [placeholder, setPlaceholder] = useState<boolean>(true)
-  const [news , setNews] = useState([])
-  const [event , setEvent] = useState([])
-  const [request , setRequest] = useState([])
-  const [organization , setOrganization] = useState([])
+  const [news, setNews] = useState([])
+  const [event, setEvent] = useState([])
+  const [request, setRequest] = useState([])
+  const [organization, setOrganization] = useState([])
   const [term, setTerm] = useState()
   const [error, setError] = useState()
 
-  useEffect(()=>{
-    if(!placeholder){
-      newsSearch();
-      eventSearch();
+  useEffect(() => {
+    if (!placeholder) {
+      newsSearch()
+      eventSearch()
       orgniazationSearch()
     }
   }, [term])
 
-
   const newsSearch = () => {
-    Axios
-      .get(`/api/news/search?term=${term}`)
-      .then(data=>data.data)
-      .then(news=>{
+    Axios.get(`/api/news/search?term=${term}`)
+      .then(data => data.data)
+      .then(news => {
         setNews(richTextToDisplayText(news))
       })
-      .catch(e=>{
+      .catch(e => {
         setError(e)
       })
   }
   const eventSearch = () => {
-    Axios
-      .get(`/api/event/search?term=${term}`)
-      .then(data=>data.data)
-      .then(events=>{
+    Axios.get(`/api/event/search?term=${term}`)
+      .then(data => data.data)
+      .then(events => {
         setEvent(events)
       })
-      .catch(e=>{
+      .catch(e => {
         setError(e)
       })
   }
 
   const orgniazationSearch = () => {
-    Axios
-      .get(`/api/organization/search?term=${term}`)
-      .then(data=>data.data)
-      .then(organization=>{
+    Axios.get(`/api/organization/search?term=${term}`)
+      .then(data => data.data)
+      .then(organization => {
         setOrganization(organization)
       })
-      .catch(e=>{
+      .catch(e => {
         setError(e)
       })
   }
-  const handleChange =(term:string)=>{
+  const handleChange = (term: string) => {
     setTerm(term)
     setPlaceholder(false)
   }
 
-
   // todo: a special type of header with the search bar embedded inside
-  return loading || (
-    <>
-      <Header  title={'Search'}/>
-      <Text>{t`app-name`}: VolunteerSearch Screen (in LayoutDefault)</Text>
-      <View>
-        <View style={searchStyle.searchContainer}>
-          <Input
-            style={searchStyle.searchInput}
-            placeholder={'search'}
-            shake
-            onChangeText={handleChange}
-          />
-        </View>
-        {
-          placeholder ? (
+  return (
+    loading || (
+      <>
+        <Header title={'Search'} />
+        <Text>{t`app-name`}: VolunteerSearch Screen (in LayoutDefault)</Text>
+        <View>
+          <View style={searchStyle.searchContainer}>
+            <Input
+              style={searchStyle.searchInput}
+              placeholder={'search'}
+              shake
+              onChangeText={handleChange}
+            />
+          </View>
+          {placeholder ? (
             <View>
-              <Text>
-                Explorer the world of SPVA!
-              </Text>
+              <Text>Explorer the world of SPVA!</Text>
             </View>
-          ):(
+          ) : (
             <View>
               <ScrollView horizontal>
-                {
-                  news.map((n:INewsResponse,k)=>(
-                    <NewsToday
-                      share={n.share.length}
-                      comment={n.comments.length}
-                      likes={n.likes.length}
-                      description={n.description}
-                      title={n.title}
-                      img={{uri:`/api/news/${n._id}/pic`}}
-                      key={k}
-                      _id={n._id}
-                    />
-                  ))
-                }
+                {news.map((n: INewsResponse, k) => (
+                  <NewsToday
+                    share={n.share.length}
+                    comment={n.comments.length}
+                    likes={n.likes.length}
+                    description={n.description}
+                    title={n.title}
+                    img={{ uri: `/api/news/${n._id}/pic` }}
+                    key={k}
+                    _id={n._id}
+                  />
+                ))}
               </ScrollView>
             </View>
-          )
-        }
-      </View>
-    </>
+          )}
+        </View>
+      </>
+    )
   )
 }
 
