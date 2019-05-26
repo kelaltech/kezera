81cf82de119cde0729fc4e200a652db39aa6d0c2
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-05-26 11:53:23 +0300

    Try linux new line character on the staging heroku build

--- api/modules/certificate/certificate.controller.ts
+++ api/modules/certificate/certificate.controller.ts
@@ -95,7 +95,7 @@ export class CertificateController extends KoaController {
     const descriptionWords = certificate.description.split(' ')
     for (let i = 0; i < descriptionWords.length; i++) {
       description += descriptionWords[i] + ' '
-      if ((i + 1) % 13 == 0) description += '\n'
+      if ((i + 1) % 13 == 0) description += '\r'
     }
 
     return new Promise<Stream>(async (resolve, reject) => {
