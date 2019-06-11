487601ee7fba127888c8dcc07c1b43478a12c92e
anteneh <anteneh845@gmail.com>
2019-06-11 08:08:48 -0400

    Added image compression and modified Request add form layout

--- web/src/shared/components/request/request-card.tsx
+++ web/src/shared/components/request/request-card.tsx
@@ -7,7 +7,7 @@ import FundCard from '../fundraising/fund-card'
 import TaskCard from '../task/task-card'
 import { useAccountState } from '../../../app/stores/account/account-provider'
 import useLocale from '../../hooks/use-locale/use-locale'
-import { Anchor, Button, Card, Content, Flex, FlexSpacer, Title } from 'gerami'
+import { Anchor, Block, Button, Card, Content, Flex, FlexSpacer, Title } from 'gerami'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface IRequestCard {
@@ -15,7 +15,7 @@ interface IRequestCard {
   type?:string
 }
 function RequestCard({ request }: IRequestCard) {
-  switch (request.type) {
+ /* switch (request.type) {
     case 'Fundraising':
       return <FundCard request={request} />
     case 'Task':
@@ -26,7 +26,91 @@ function RequestCard({ request }: IRequestCard) {
       return <TaskCard request={request} />
     default:
       return null
-  }
+  }*/
+  const { account } = useAccountState()
+  return(
+    <Card
+      imgSrc={request.coverUri}
+      className={'requestCard'}
+      children={
+        <>
+          <Title size={'M'}>
+            <Anchor to={`/request/${request._id}`}>
+              {request.name}
+            </Anchor>
+          </Title>
+          <span>
+             <FontAwesomeIcon
+               color={'blue'}
+               icon={'donate'}
+               className={'TrashIcon'}
+             />&emsp; {request.type}
+           </span>
+          <br/>
+          <span>
+            <FontAwesomeIcon
+              color={'blue'}
+              icon={'calendar'}
+            />&emsp;
+            {new Date(request.expires).toDateString()}
+           </span>
+          <br/>
+          {request.type=='Fundraising'?
+            <>
+              <FontAwesomeIcon
+                icon={'money-bill'}
+                color={'green'}
+              />&emsp;
+              10,000 etb
+            </>
+            :''}
+          {request.type=='Material'?
+            <>
+              <FontAwesomeIcon
+                color={'grey'}
+                icon={'tshirt'}
+              />&esmp;
+              10
+            </>:''}
+          {request.type=='Task'?
+            <>
+              <FontAwesomeIcon
+                color={'blue'}
+                icon={'tasks'}
+              />&emsp;
+              Cleaning
+            </>:''}
+          {request.type=='Organ'?
+            <>
+              <FontAwesomeIcon
+                color={'rgb(223,72,61)'}
+                icon={'hand-holding-heart'}
+              />&emsp;
+              2
+            </>:''}
+          {account && account.role === 'ORGANIZATION' ?
+            <Block className={'right'}>
+               <span className={'requestDeleteButton'} onClick={()=>alert('Abebe')}>
+                 <FontAwesomeIcon
+                   color={'blue'}
+                   icon={'pencil-alt'}
+                   className={'TrashIcon'}
+                 />
+               </span>
+              &emsp;&emsp;
+              <span className={'requestDeleteButton'} onClick={()=>alert('Abebe')}>
+                 <FontAwesomeIcon
+                   color={'red'}
+                   icon={'trash'}
+                   className={'TrashIcon'}
+                 />
+               </span>
+            </Block>
+            :''}
+        </>
+      }
+    />
+  )
 }
 
 // @ts-ignore
